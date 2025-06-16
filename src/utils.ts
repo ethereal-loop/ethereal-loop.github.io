85dfc425d805a7c9a44c825d46e6410e3c3bbf22
matan h <matan.honig2@gmail.com>
2025-06-16 19:41:57 +0300

    fix a bug related to appwrite files accses

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ export async function buildMusicUrl(musicName: string): Promise<string> {
   const projectId = import.meta.env.VITE_APPWRITE_PROJECT_ID;
   const bucketId = import.meta.env.VITE_APPWRITE_BUCKET_ID;
   const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
-  return `https://${endpoint}/v1/storage/buckets/${bucketId}/files/${fileId}/view?project=${projectId}&mode=admin`;
+  return `https://${endpoint}/v1/storage/buckets/${bucketId}/files/${fileId}/view?project=${projectId}`;
 }
 
 export function getURL(musicName: string, source: string): string {
